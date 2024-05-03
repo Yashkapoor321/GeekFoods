b15cc135eab01c7e95b32349848e4fae3af294f4
Yash Kapoor <yashkapur1996@gmail.com>
2024-05-03 16:31:15 +0530

    GeekFood app is Created

---
+++ GeekFood/src/Components/Header/Header.jsx
@@ -0,0 +1,28 @@
+
+const Header = () => {
+    return (
+        <>
+        <header className="flex">
+
+       <div>
+        <h1>GeekFoods</h1>
+       </div>
+
+       <div>
+        <h2>Home</h2>
+        <h2>Quote</h2>
+        <h2>Resturants</h2>
+        <h2>Foods</h2>
+        <h2>Contact</h2>
+       </div>
+
+       <div>
+        <button>Get Started</button>
+       </div>
+
+       </header>
+        </>
+      )
+    }
+    
+ export default Header;
